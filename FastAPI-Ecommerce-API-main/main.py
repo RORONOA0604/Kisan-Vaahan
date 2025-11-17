b6a0db0d01d2b975719415ee49b9bcdeefe42ea0
RORONOA0604 <roronovaz597@gmail.com>
2025-11-17 16:16:55 +0530

    arranged structure

--- FastAPI-Ecommerce-API-main/main.py
+++ FastAPI-Ecommerce-API-main/main.py
@@ -1,4 +1,12 @@
 from app.main import app
+from fastapi.middleware.cors import CORSMiddleware
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=["http://localhost:5500", "http://localhost:3000", "http://localhost:8000"], # add your frontend origin(s)
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     app.run()
